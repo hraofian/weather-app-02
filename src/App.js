6d3dfa326eb1dacc74d7b2a8f360f31b0c966446
hadi <hadi@hadi.com>
2025-02-11 11:40:21 +0330

    edith 01

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
-    <div className='App'>
-      <h1>Happy Coding!</h1>
+    <div className="App">
+      <h1>Happy Coding! </h1>
+      <h1>Hello </h1>
       <h2>ReactJs Weather App | By Code Explained</h2>
     </div>
   );
