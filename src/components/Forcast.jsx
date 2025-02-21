862a3760bd5ea870870d17f055ffe60eb337b3c1
hadi <hadi@hadi.com>
2025-02-21 11:25:53 +0330

    11 coding forcast

--- src/components/Forcast.jsx
+++ src/components/Forcast.jsx
@@ -1,5 +1,14 @@
-function Forcast() {
-  return <div>Forcast</div>;
+import "../styles/components/Forecast.scss";
+
+function Forcast({ title, type, data }) {
+  return (
+    <div className="Forecast">
+      <div className="forcast-container">
+        <h3>{title}</h3>
+        <div className="widget-container"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Forcast;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,13 +2,14 @@ import "../styles/components/Main.scss";
 import CurrentWeather from "./CurrentWeather";
 import Forcast from "./Forcast";
 import "../styles/components/CurrentWeather.scss";
+import { getDailyForcast, getHourlyForcast } from "../api";
 
 function Main() {
   return (
     <div className="Main">
       <CurrentWeather />
-      <Forcast type="hourly" title="HOURLY FORCAST" data={} />
-      <Forcast type="daily" title="21 DAYS FORCAST" data={} />
+      <Forcast type="hourly" title="HOURLY FORCAST" data={getHourlyForcast()} />
+      <Forcast type="daily" title="21 DAYS FORCAST" data={getDailyForcast()} />
     </div>
   );
 }
