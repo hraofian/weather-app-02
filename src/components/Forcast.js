91fe6ab48729eeedd03324c709420f8c475b4a7b
hadi <hadi@hadi.com>
2025-02-22 23:46:35 +0330

    18 codding scrolling- need try

--- src/components/Forcast.js
+++ src/components/Forcast.js
@@ -1,23 +1,26 @@
 import "../styles/components/Forecast.scss";
 import HourlyForcastWidget from "./HourlyForcastWidget";
 import DailyForcastWidget from "./DailyForcastWidget";
+import HorizontallyScrollable from "./HorizontallyScrollable";
 
 function Forcast({ title, type, data }) {
   return (
     <div className="Forecast">
       <div className="forcast-container">
         <h3>{title}</h3>
-        <div className="widget-container">
-          {data.map((singleData) => (
-            <div>
-              {type === "hourly" ? (
-                <HourlyForcastWidget data={singleData} />
-              ) : (
-                <DailyForcastWidget data={singleData}/>
-              )}
-            </div>
-          ))}
-        </div>
+        <HorizontallyScrollable>
+          <div className="widget-container">
+            {data.map((singleData) => (
+              <div key={singleData.date || singleData.day}>
+                {type === "hourly" ? (
+                  <HourlyForcastWidget data={singleData} />
+                ) : (
+                  <DailyForcastWidget data={singleData} />
+                )}
+              </div>
+            ))}
+          </div>
+        </HorizontallyScrollable>
       </div>
     </div>
   );
