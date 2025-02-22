f64509524848193f82d86b90ef71b67e39e237cb
hadi <hadi@hadi.com>
2025-02-22 23:01:41 +0330

    16 start horizontally scrolling components

--- src/components/DailyForcastWidget.tsx
+++ src/components/DailyForcastWidget.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import WeatherIcon from "./WeatherIcon.tsx";
 
-function DailyForcastWidget() {
+function DailyForcastWidget({ data }) {
+  const {
+    day,
+    icon,
+    summary,
+    temperature_max,
+    temperature_min,
+    precipitation,
+  } = data;
+
+  //date format
+  const now_date = {
+    day: new Intl.DateTimeFormat(navigator.language, {
+      weekday: "short",
+      day: "2-digit",
+      month: "2-digit",
+    }).format(new Date()),
+  };
+
+  const weather_date = {
+    day: new Intl.DateTimeFormat(navigator.language, {
+      weekday: "short",
+      day: "2-digit",
+      month: "2-digit",
+    }).format(new Date(day)),
+  };
+
+  weather_date.day  = now_date.day === weather_date.day ? "Today" : weather_date.day
+
+  console.log(data);
   return (
-    <div>DailyForcastWidget</div>
-  )
+    <div className="widget">
+      <div className="day">{weather_date.day}</div>
+      <div className="icon-temp">
+        <div className="icon">
+          <WeatherIcon iconNumber={icon} summary={summary} />
+        </div>
+        <div className="max">Max {Math.round(temperature_max)} ℃</div>
+        <div className="min">Min {Math.round(temperature_min)} ℃</div>
+      </div>
+      <div className="precipitation">
+        {Math.round(precipitation.total)} mm/h
+      </div>
+    </div>
+  );
 }
 
-export default DailyForcastWidget
+export default DailyForcastWidget;
